455806107626fa20224b0c99d60cfe40508c5456
Pennyv99 <126306243+Pennyv99@users.noreply.github.com>
2026-01-30 11:55:03 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import * as d3 from 'd3'
+
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './theme-chalk/index.scss'
+
+import App from './App.vue'
+import router from './router'
+
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus)
+
+app.mount('#app')
